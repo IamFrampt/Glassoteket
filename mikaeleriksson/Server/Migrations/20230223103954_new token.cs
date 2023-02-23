544d732d77b101434fbbb6615ba02ab3c532c5f4
Frampt <mikael.eriksson@iths.se>
2023-02-23 11:40:21 +0100

    hej

---
+++ mikaeleriksson/Server/Migrations/20230223103954_new token.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace mikaeleriksson.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class newtoken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
