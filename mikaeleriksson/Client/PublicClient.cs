cc1e0aaf8674de5701ec4d23ddb9460b231aa6cb
Frampt <mikael.eriksson@iths.se>
2023-02-23 11:07:04 +0100

    commit

--- mikaeleriksson/Client/PublicClient.cs
+++ mikaeleriksson/Client/PublicClient.cs
@@ -2,5 +2,12 @@
 {
     public class PublicClient
     {
+        public HttpClient client { get; set; }
+
+        public PublicClient(HttpClient client)
+        {
+            this.client = client;
+        }
+
     }
 }
